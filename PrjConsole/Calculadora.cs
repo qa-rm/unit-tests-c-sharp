b3840db429bafbdea269513cc3c49fe27e786f9b
Rafael Motta <rafael.meneguim.motta@gmail.com>
2024-12-30 17:29:31 -0300

    melhorias

--- PrjConsole/Calculadora.cs
+++ PrjConsole/Calculadora.cs
@@ -3,37 +3,39 @@
     public class Calculadora
     {
         private List<string> listaHistorico;
+        private string data;
 
-        public Calculadora()
+        public Calculadora(string data)
         {
             listaHistorico = new List<string>();
+            this.data = data;
         }
 
         public int somar(int val1, int val2)
         {
             int res1 = val1 + val2;
-            listaHistorico.Insert(0, "Teste de somar ("+val1+" + "+val2+" = "+res1);
+            listaHistorico.Insert(0, "Teste de somar ("+val1+" + "+val2+" = "+res1+" - Data: "+data);
             return res1;
         }
 
         public int subtrair(int val1, int val2)
         {
             int res1 = val1 - val2;
-            listaHistorico.Insert(0, "Teste de subtrair ("+val1+" - "+val2+" = "+res1);
+            listaHistorico.Insert(0, "Teste de subtrair ("+val1+" - "+val2+" = "+res1+" - Data: "+data);
             return res1;
         }
 
         public int multiplicar(int val1, int val2)
         {
             int res1 = val1 * val2;
-            listaHistorico.Insert(0, "Teste de multiplicar ("+val1+" * "+val2+" = "+res1);
+            listaHistorico.Insert(0, "Teste de multiplicar ("+val1+" * "+val2+" = "+res1+" - Data: "+data);
             return res1;
         }
 
         public int dividir(int val1, int val2)
         {
             int res1 = val1 / val2;
-            listaHistorico.Insert(0, "Teste de dividir ("+val1+" / "+val2+" = "+res1);
+            listaHistorico.Insert(0, "Teste de dividir ("+val1+" / "+val2+" = "+res1+" - Data: "+data);
             return res1;
         }
 
--- PrjXUnitTests/CalculadoraTests.cs
+++ PrjXUnitTests/CalculadoraTests.cs
@@ -4,13 +4,19 @@ namespace PrjXUnitTests
 {
     public class CalculadoraTests
     {
-        Calculadora calc = new Calculadora();
+        private Calculadora construirClasse()
+        {
+            Calculadora calc = new Calculadora("30 de dez 2024 17:25");
+            return calc;
+        }
 
         [Theory]
         [InlineData (1, 2, 3)]
         [InlineData (4, 5, 9)]
         public void TestSomar(int val1, int val2, int res)
         {
+            Calculadora calc = construirClasse();
+            
             int resultado = calc.somar(val1, val2);
 
             Assert.Equal(res, resultado);
@@ -20,7 +26,9 @@ namespace PrjXUnitTests
         [InlineData (1, 2, -1)]
         [InlineData (4, 5, -1)]
         public void TestSubtrair(int val1, int val2, int res)
-        {            
+        {
+            Calculadora calc = construirClasse();
+
             int resultado = calc.subtrair(val1, val2);
 
             Assert.Equal(res, resultado);
@@ -30,7 +38,9 @@ namespace PrjXUnitTests
         [InlineData (1, 2, 2)]
         [InlineData (4, 5, 20)]
         public void TestMultiplicar(int val1, int val2, int res)
-        {           
+        {
+            Calculadora calc = construirClasse();
+
             int resultado = calc.multiplicar(val1, val2);
 
             Assert.Equal(res, resultado);
@@ -40,7 +50,9 @@ namespace PrjXUnitTests
         [InlineData (1, 2, 0)]
         [InlineData (4, 5, 0)]
         public void TestDividir(int val1, int val2, int res)
-        {           
+        {
+            Calculadora calc = construirClasse();
+
             int resultado = calc.dividir(val1, val2);
 
             Assert.Equal(res, resultado);
@@ -49,6 +61,8 @@ namespace PrjXUnitTests
         [Fact]
         public void TestDividirPorZero()
         {
+            Calculadora calc = construirClasse();
+
             Assert.Throws<DivideByZeroException>(
                 () => calc.dividir(3, 0)
             );
@@ -57,6 +71,8 @@ namespace PrjXUnitTests
         [Fact]
         public void TestHistorico()
         {
+            Calculadora calc = construirClasse();
+            
             calc.somar(1, 2);
             calc.somar(3, 4);
             calc.somar(5, 6);
