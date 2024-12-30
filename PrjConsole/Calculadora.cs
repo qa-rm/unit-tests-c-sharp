e40291e115a8b8e6b8441d0ecf585c4b80c4a865
Rafael Motta <rafael.meneguim.motta@gmail.com>
2024-12-30 17:17:33 -0300

    versao finalizada

--- PrjConsole/Calculadora.cs
+++ PrjConsole/Calculadora.cs
@@ -2,29 +2,45 @@
 {
     public class Calculadora
     {
+        private List<string> listaHistorico;
+
+        public Calculadora()
+        {
+            listaHistorico = new List<string>();
+        }
+
         public int somar(int val1, int val2)
         {
-            return val1 + val2;
+            int res1 = val1 + val2;
+            listaHistorico.Insert(0, "Teste de somar ("+val1+" + "+val2+" = "+res1);
+            return res1;
         }
 
         public int subtrair(int val1, int val2)
         {
-            return val1 - val2;
+            int res1 = val1 - val2;
+            listaHistorico.Insert(0, "Teste de subtrair ("+val1+" - "+val2+" = "+res1);
+            return res1;
         }
 
         public int multiplicar(int val1, int val2)
         {
-            return val1 * val2;
+            int res1 = val1 * val2;
+            listaHistorico.Insert(0, "Teste de multiplicar ("+val1+" * "+val2+" = "+res1);
+            return res1;
         }
 
         public int dividir(int val1, int val2)
         {
-            return val1 / val2;
+            int res1 = val1 / val2;
+            listaHistorico.Insert(0, "Teste de dividir ("+val1+" / "+val2+" = "+res1);
+            return res1;
         }
 
         public List<string> historico()
         {
-            return new List<string>();
+            listaHistorico.RemoveRange(3, listaHistorico.Count - 3);
+            return listaHistorico;
         }
     }
 }
